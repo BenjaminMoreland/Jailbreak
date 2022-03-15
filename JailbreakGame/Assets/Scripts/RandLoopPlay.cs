43e5b66a1ca236e554b160392a5dee63fd469030
Logan Laurance <logan.laurance@digipen.edu>
2022-03-14 22:52:01 -0700

    Tile stuff and sprites
    Separated collision from bricks, added trigger collision for ladders and now works

--- JailbreakGame/Assets/Scripts/RandLoopPlay.cs
+++ JailbreakGame/Assets/Scripts/RandLoopPlay.cs
@@ -8,30 +8,19 @@ using UnityEngine.Audio;
 public class RandLoopPlay : MonoBehaviour
 {
     private AudioSource gAud;
-    public AudioClip chainAud;
     // Start is called before the first frame update
     void Start()
     {
-        gAud = GetComponent<AudioSource>();
+        gAud = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //default playback method for testing
-        if (Input.GetKeyDown(KeyCode.P))
+        int randNum = Random.Range(0, 480);
+        if(randNum == 55)
         {
-            PlaySound();
-        }
-    }
-
-    public void PlaySound()
-    {
-        int randomPlay = Random.Range(1, 2);
-
-        if(randomPlay == 2)
-        {
-            gAud.PlayOneShot(chainAud);
+            gAud.Play();
         }
     }
 }
